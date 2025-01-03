bf519bf8fc7acaddd45c62467ca871ea103c7039
sazzad himel <himel.s.hossain@gmail.com>
2025-01-03 14:00:09 +0600

    deploy2 commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
@@ -11,29 +12,29 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <a href="/IBC2025-Conference-Website/"><img src={logo} alt="Logo" className="logo" /></a>
+            <Link to="/IBC2025-Conference-Website/"><img src={logo} alt="Logo" className="logo" /></Link>
             <div className={`menu ${isMenuOpen ? "open" : ""}`}>
                 <ul>
-                    <li><a href="/IBC2025-Conference-Website/">HOME</a></li>
-                    <li><a href="/IBC2025-Conference-Website/about-us">ABOUT US</a></li>
-                    <li><a href="/IBC2025-Conference-Website/abstract">ABSTRACT</a></li>
-                    <li><a href="/IBC2025-Conference-Website/organizers">ORGANIZERS</a></li>
-                    <li><a href="/IBC2025-Conference-Website/venue">VENUE</a></li>
-                    <li><a href="/IBC2025-Conference-Website/contact-us">CONTACT US</a></li>
+                <li><Link to="/IBC2025-Conference-Website/">HOME</Link></li>
+                <li><Link to="/IBC2025-Conference-Website/about-us">ABOUT US</Link></li>
+                <li><Link to="/IBC2025-Conference-Website/abstract">ABSTRACT</Link></li>
+                <li><Link to="/IBC2025-Conference-Website/organizers">ORGANIZERS</Link></li>
+                <li><Link to="/IBC2025-Conference-Website/venue">VENUE</Link></li>
+                <li><Link to="/IBC2025-Conference-Website/contact-us">CONTACT US</Link></li>
                 </ul>
             </div>
             <div className="hamburger" onClick={toggleMenu}>
                 {isMenuOpen ? (
-                    <span className="close-icon">✖</span>
+                <span className="close-icon">✖</span>
                 ) : (
-                    <div className="bars-icon">
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                    </div>
+                <div className="bars-icon">
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
                 )}
             </div>
-        </nav>
+            </nav>
     );
 };
 
