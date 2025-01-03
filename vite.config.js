b306874edad615191f3908333c0807c92fbe727c
sazzad himel <himel.s.hossain@gmail.com>
2025-01-03 13:19:20 +0600

    forth commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/IBC2025-Conference-Website/',
+  build: { outDir: 'build' },  // Use this if deploying to a subdirectory
 })
+
